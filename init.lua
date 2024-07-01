144bbc933efd0e7f186221d8be5325eb47de6b95
AWNathan <aaratrick.r2ab@gmail.com>
2024-07-01 10:51:57 +0530

    Configuring LuaSnip

--- init.lua
+++ init.lua
@@ -908,6 +908,19 @@ require('lazy').setup({
   },
 })
 
+-- LuaSnip
+
+require('luasnip').config.set_config { -- Setting LuaSnip config
+
+  -- Enable autotriggered snippets
+  enable_autosnippets = true,
+
+  -- Use Tab (or some other key if you prefer) to trigger visual selection
+  store_selection_keys = '<Tab>',
+}
+
+require('luasnip.loaders.from_lua').lazy_load { paths = '~/.config/nvim/LuaSnip/' }
+
 -- Folding
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
