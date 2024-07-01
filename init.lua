caa5d2d0d77e5e7c607883e69bdbfd9f8d875094
AWNathan <aaratrick.r2ab@gmail.com>
2024-07-01 22:17:51 +0530

    added VimTex

--- init.lua
+++ init.lua
@@ -863,6 +863,15 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      --  vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -923,6 +932,10 @@ require('luasnip').config.set_config { -- Setting LuaSnip config
 }
 
 require('luasnip.loaders.from_lua').lazy_load { paths = '~/.config/nvim/LuaSnip/' }
+
+-- Keymap to lazy load snippets, so that snippets can be changed while working on files.
+
+vim.keymap.set('n', '<Leader>L', '<Cmd>lua require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip/"})<CR>')
 
 -- Folding
 vim.opt.foldmethod = 'expr'
